cf4580de8f0313019d142d77b57ed9c6346982c9
Medine Burunkaya <medineburunkaya032@gmail.com>
2023-06-08 19:42:04 +0300

    icon eklendi

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-
 class MyButton extends StatelessWidget {
   final color;
   final textColor;
   final String? buttonText;
   final buttonTapped;
-
   MyButton({this.color, this.textColor, this.buttonText, this.buttonTapped});
   @override
   Widget build(BuildContext context) {
